21bbf58a5e8d880bcefdf339c4b41f985293740a
andhikaibn <astroid2200@gmail.com>
2024-03-21 16:52:04 +0700

    latihan Java

---
+++ Latihanvar.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihanvar;
+
+/**
+ *
+ * @author ANDIKA
+ */
+public class Latihanvar {
+
+    public static void main(String[] args) {
+        //
+        int JariJari;
+        double Keliling;
+        final double PI=3.14;
+        
+        JariJari =30;
+        Keliling =2*PI*JariJari;
+        System.out.println("Keliling Lingkaran" +Keliling);
+        System.out.println("==============");
+        
+        
+    }
+}
