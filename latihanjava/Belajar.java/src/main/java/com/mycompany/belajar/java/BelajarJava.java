883d2cb0e15574e2cfbc44313e5750ba78e7aacf
andhikaibn <astroid2200@gmail.com>
2024-03-27 22:47:31 +0700

    first commit

--- latihanjava/Belajar.java/src/main/java/com/mycompany/belajar/java/BelajarJava.java
+++ latihanjava/Belajar.java/src/main/java/com/mycompany/belajar/java/BelajarJava.java
@@ -1,14 +1,18 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package com.mycompany.belajar.java;
+package com.mycompany.java;
 
 /**
  *
- * @author ANDIKA
+ * @author ezran
  */
-public class Belajar {
-    public String x = "Pintar";
-    public String y = "Java";
+public class Pintar {
+    
+    public static void main (String[] args){
+        Belajar panggil = new Belajar();
+        System.out.println("Panggil X : " + panggil.x);
+        System.out.println("Panggil Y : " + panggil.y);        
+    }
 }
