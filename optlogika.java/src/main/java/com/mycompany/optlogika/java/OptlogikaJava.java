c53b8ae66db89806fe858ee7d6f9420f71208fd2
andhikaibn <astroid2200@gmail.com>
2024-03-27 19:13:48 +0700

    push latihan java pertama

---
+++ optlogika.java/src/main/java/com/mycompany/optlogika/java/OptlogikaJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.optlogika.java;
+
+/**
+ *
+ * @author ANDIKA
+ */
+public class OptlogikaJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
