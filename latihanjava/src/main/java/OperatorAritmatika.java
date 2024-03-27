2284f6362fc15bd4c7bbaaab9e4feab5600a8879
andhikaibn <astroid2200@gmail.com>
2024-03-27 22:55:42 +0700

    test commit

---
+++ latihanjava/src/main/java/OperatorAritmatika.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ANDIKA
+ */
+public class OperatorAritmatika {
+    
+ public static void main ( String [] args ) {
+// Deklarasi nilai variabel
+int i = 9 ;
+int j = 4 ;
+// Menampilkan nilai Variabel
+System.out.println("Nilai variabel : ");
+System.out.println(" i = " + i);
+System.out.println(" j = " + j);
+// Menggunakan Operator penjumlahan
+System.out.println("Penjumlahan : ");
+System.out.println(" i + j = " +(i + j ));
+// Menggunakan Operator pengurangan
+System.out.println("Pengurangan : ");
+System.out.println(" i - j = " + (i - j ));
+// Menggunakan Operator perkalian
+System.out.println("Perkalian : ");
+System.out.println(" i * j = " + (i * j ));
+// Menggunakan Operator pembagian
+System.out.println("Pembagian : ");
+System.out.println(" i / j = " + (i / j ));
+// Menggunakan Operator modulus
+System.out.println("Modulus : ");
+System.out.println(" i % j = " + (i % j ));
+
+}   
+}
